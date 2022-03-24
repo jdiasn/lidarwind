6c8a7bcf21dd22ba578b480c5937555eb298bf7e
Dias Neto <jdiasn@gmail.com>
2022-03-24 18:40:29 +0100

    Requirements correction (#3)
    * Moving requirements to root
    
    * fix: Removing dependency on datetime (core module)
    
    * fix: typo in readme filename
    
    * Feat: using external requirements from the file .txt
    
    * Refactor: Defining requirements' versions
    
    * Refactor: correction of the requirement versions

--- setup.py
+++ setup.py
@@ -4,13 +4,14 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open('README.md') as readme_file:
     readme = readme_file.read()
 
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', ]
+with open('requirements.txt') as requirements_file:
+    requirements = requirements_file.read()
 
 test_requirements = ['pytest>=3', ]
 
