a7493e1e718ed3af3482f999baef5062e2aae372
Dias Neto <jdiasn@gmail.com>
2022-03-25 17:35:15 +0100

    Feat/pyproject (#4)
    * Feat: intrudocing pyproject.toml
    
    * Refactor: cleaning setup.py
    
    all metadata moved to pyproject.toml
    
    * Ignoring version.py
    
    * Cleanning setup.cfg, we do not use bumpversion

--- setup.py
+++ setup.py
@@ -4,47 +4,9 @@
 
 from setuptools import setup, find_packages
 
-with open('README.md') as readme_file:
-    readme = readme_file.read()
-
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
-with open('requirements.txt') as requirements_file:
-    requirements = requirements_file.read()
-
-test_requirements = ['pytest>=3', ]
 
 setup(
-    author="Jose Dias",
-    author_email='jdiasn@gmail.com',
-    python_requires='>=3.6',
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-    ],
-    description="LIDAR procedures",
-    entry_points={
-        'console_scripts': [
-            'lidarSuit=lidarSuit.cli:main',
-        ],
-    },
-    install_requires=requirements,
-    license="MIT license",
-    long_description=readme + '\n\n' + history,
     include_package_data=True,
-    keywords='lidarSuit',
-    name='lidarSuit',
-    packages=find_packages(include=['lidarSuit', 'lidarSuit.*']),
-    test_suite='tests',
-    tests_require=test_requirements,
-    url='https://github.com/CMTRACE-team/lidarSuit',
-    version='0.0.1',
+    packages=find_packages(include=["lidarSuit", "lidarSuit.*"]),
     zip_safe=False,
 )
