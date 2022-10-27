7cbab24252136187e34294b6569f33e39eacf831
Dias Neto <jdiasn@gmail.com>
2022-10-27 23:13:31 +0200

    Adding io.py (#39)
    * Adding io.py
    
    * BUG fix to run xarray 2022.06.0 (#38)
    
    * Adding test IO
    
    * Fix: fixing compatibility xarray issue
    
    * Adding vertical data check
    
    * updating sample data path
    
    * Fix: correcting gdwon dependence
    
    * Removing old requirements
    
    * Fix: correcting tag dependence
    
    * Updating xarray dependence
    
    * Feat: functionalities to download data samples
    
    * Fixing dependence
    
    * Solving gdwon dependece (documentation) (#40)
    
    * Refactor/big file compatibility (#41)
    
    * Solving gdwon dependece (documentation)
    
    * Fix: gdown dependence
    
    * Refactor/big file compatibility (#42)
    
    * Solving gdwon dependece (documentation)
    
    * Fix: gdown dependence
    
    * Refactor: improving scan reference time assignment
    
    * Feat: adding long dbs example
    
    * Fix: read the docs dependence (#43)
    
    * Adding io.py
    
    * Deprecate get lidar data (#44)
    
    * Adding minor inline info
    
    * Warning for getLidarData
    
    Co-authored-by: Dias Neto <jdiasn@gmail.com>
    
    * Refactor/tag package version (#45)
    
    * refactor: tag gdown and lidarSuit versions
    
    * fixing sintaxe
    
    * Adding datatree dependencie
    
    * correcting typo and setting datatree version
    
    * Code coverage (#50)
    
    * Reporting coverage
    
    * Adding pytest-cov into test optionals
    
    * Adding temporary badge to be updated later
    
    * Defining cache key
    
    * Updating badge
    
    * Adding io.py
    
    * Deprecate get lidar data (#44)
    
    * Adding minor inline info
    
    * Warning for getLidarData
    
    Co-authored-by: Dias Neto <jdiasn@gmail.com>
    
    * Adding datatree dependencie
    
    * correcting typo and setting datatree version
    
    Co-authored-by: Guilherme Castelão <guilherme@castelao.net>

---
+++ lidarSuit/io.py
@@ -0,0 +1,40 @@
+import datatree
+import pandas as pd
+import xarray as xr
+
+
+def open_sweep(file_name):
+    """Windcube's data reader
+
+    It opens and reads the original NetCDF output
+    from the Windcube lidar.
+
+    Parameters
+    ----------
+
+    file_name : str
+        path to the file that will be opened
+
+    Returns
+    -------
+    tmpData : xarray.DataSet
+
+        a dataset from the original NetCDF files
+    """
+
+    raw_data = datatree.open_datatree(file_name, decode_times=False)
+
+    assert (
+        "sweep_group_name" in raw_data
+    ), "missing sweep group variable in input file"
+    sweep_group_name = raw_data["sweep_group_name"].values[0]
+    ds = raw_data[f"/{sweep_group_name}"].to_dataset()
+
+    assert "time_reference" in ds, "missing time_reference in input sweep"
+    # Guarantee that it is a valid datetime
+    reference_time = pd.to_datetime(ds["time_reference"].values).isoformat()
+    ds["time"].attrs["units"] = f"seconds since {reference_time}"
+
+    ds = xr.decode_cf(ds)
+
+    return ds
