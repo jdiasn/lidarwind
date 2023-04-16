0b11bdddaeffe700a9dccb16898849977cbf5348
Dias Neto <jdiasn@gmail.com>
2023-04-16 22:55:47 +0200

    Test: deactivating some tests to identify seg fault

--- tests/test_IO.py
+++ tests/test_IO.py
@@ -1,18 +1,20 @@
-import pytest
+# import pytest
 
-import lidarwind as lst
+# import lidarwind as lst
 
-from .data import data_filenames, get_sample_data
-
-
-@pytest.fixture
-def test_DataOperations(data_filenames):
-
-    tmp_object = lst.DataOperations(data_filenames)
-
-    return tmp_object.merged_data
+# from .data import data_filenames, get_sample_data
 
 
-def test_GetRestructuredData(test_DataOperations):
+# @pytest.fixture
+# @pytest.mark.skip("deactivating to isolate sef fault error")
+# def test_DataOperations(data_filenames):
 
-    restruct_data = lst.GetRestructuredData(test_DataOperations)
+#     tmp_object = lst.DataOperations(data_filenames)
+
+#     return tmp_object.merged_data
+
+
+# @pytest.mark.skip("deactivating to isolate sef fault error")
+# def test_GetRestructuredData(test_DataOperations):
+
+#     restruct_data = lst.GetRestructuredData(test_DataOperations)
