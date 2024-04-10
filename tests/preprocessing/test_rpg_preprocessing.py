29aebb0a3171a0663a6a22ca4f5abfe51f160596
Dias Neto <jdiasn@gmail.com>
2024-04-11 01:00:36 +0200

    Feat: adding test to selecting_variables

--- tests/preprocessing/test_rpg_preprocessing.py
+++ tests/preprocessing/test_rpg_preprocessing.py
@@ -21,3 +21,27 @@ def test_time_decoding_timems():
 
     with pytest.raises(AssertionError):
         rpg_radar.time_decoding(ds=xr.Dataset({"Time": [0]}))
+
+
+def test_selecting_variables_ds_type():
+
+    with pytest.raises(TypeError):
+        rpg_radar.selecting_variables(ds=np.array([1, 2]))
+
+
+def test_selecting_variables_time():
+
+    with pytest.raises(AssertionError):
+        rpg_radar.selecting_variables(ds=xr.Dataset({"Azm": [0], "Elv": [0]}))
+
+
+def test_selecting_variables_azm():
+
+    with pytest.raises(AssertionError):
+        rpg_radar.selecting_variables(ds=xr.Dataset({"Time": [0], "Elv": [0]}))
+
+
+def test_selecting_variables_elv():
+
+    with pytest.raises(AssertionError):
+        rpg_radar.selecting_variables(ds=xr.Dataset({"Time": [0], "Azm": [0]}))
