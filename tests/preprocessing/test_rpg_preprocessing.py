2660067a722c60e4c87538f410c926a3bbe195c5
Dias Neto <jdiasn@gmail.com>
2024-04-12 23:30:11 +0200

    Refactor: testing azimuth offset correction

--- tests/preprocessing/test_rpg_preprocessing.py
+++ tests/preprocessing/test_rpg_preprocessing.py
@@ -45,3 +45,15 @@ def test_selecting_variables_elv():
 
     with pytest.raises(AssertionError):
         rpg_radar.selecting_variables(ds=xr.Dataset({"Time": [0], "Azm": [0]}))
+
+
+def test_azimuth_offset_ds():
+
+    with pytest.raises(TypeError):
+        rpg_radar.azimuth_offset(ds=np.array([1, 2]))
+
+
+def test_azimuth_offset_azm():
+
+    with pytest.raises(AssertionError):
+        rpg_radar.azimuth_offset(ds=xr.Dataset({"Time": [0], "Elv": [0]}))
