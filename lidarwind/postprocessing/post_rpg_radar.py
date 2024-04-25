a98987123968d105abeae3642d08e88e3c4f478e
Dias Neto <jdiasn@gmail.com>
2024-04-25 16:47:51 +0200

    Refactor: adding variable types

--- lidarwind/postprocessing/post_rpg_radar.py
+++ lidarwind/postprocessing/post_rpg_radar.py
@@ -1,7 +1,9 @@
+import xarray as xr
+
 from ..wind_retrieval.fft_wind_retrieval import get_wind_properties
 
 
-def get_horizontal_wind(ds):
+def get_horizontal_wind(ds: xr.Dataset) -> xr.Dataset:
 
     """Horizontal wind dataset
 
@@ -26,6 +28,9 @@ def get_horizontal_wind(ds):
         dataset.
 
     """
+
+    if not isinstance(ds, xr.Dataset):
+        raise TypeError(f"{ds} is not an instance of xr.Dataset")
 
     required_variables = [
         "start_scan",
